fef673c2495df6492e2c664e826768b38a7afb5a
ruthabebeneg <124071594+ruthabebeneg@users.noreply.github.com>
2025-05-06 10:40:13 +0200

    Add files via upload

--- exam_q1.py
+++ exam_q1.py
@@ -14,7 +14,7 @@ class TagsBreakdown(MRJob):
             userID, movieID, tag, timestamp = line.strip().split('\t')
 
             # 1. Nombre de tags par film
-            yield ("film_tags3,moviid"),1
+            yield ("film_tags",movieID),1
 
         except Exception:
             pass
